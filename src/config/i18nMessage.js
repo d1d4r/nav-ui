455e0dbbb5bf61f45933c65c5ae1e7c664980288
didar <deddarnawzad@gmail.com>
2024-03-07 22:31:14 +0300

    config message for i18n

---
+++ src/config/i18nMessage.js
@@ -0,0 +1,20 @@
+const messages = {
+  en: {
+    message: {
+      home: 'home',
+      about: 'about',
+      project: 'project',
+      contact : "contact"
+    }
+  },
+  ku: {
+    message: {
+      home: 'سەرەکی',
+      about: 'دەربارە',
+      project: 'پرۆژە',
+      contact : 'پەیوەنی بکە'
+    }
+  }
+}
+
+export default messages
