6edfc3ca17e2113da0020d60cee18ec34533f744
didar <deddarnawzad@gmail.com>
2024-03-08 20:21:05 +0300

    add HeroSection with grid stack layout

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="grid grid-cols-1 grid-rows-1">
+    <div class="col-start-1 col-end-1 row-start-1 row-end-1 h-96">
+      <img
+        src="../assets/image/webCover.jpg"
+        alt="Image 1"
+        class="object-cover w-full h-full blur-sm"
+      />
+    </div>
+    <div class="z-10 col-start-1 col-end-1 row-start-1 row-end-1 p-5 m-auto border">
+      <h2 class="text-4xl font-bold text-center text-black">hi there</h2>
+      <p class="text-2xl text-center text-black w-96 word">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quisquam!
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+
+</style>
+
+
+<!-- <div class="h-96">
+      <div class="relative h-full">
+       
+        <div
+          class="absolute top-0 bottom-0 flex items-center justify-center text-white left-10 bg-inherit"
+        >
+          <div class="flex flex-col items-center justify-center h-full text-center">
+            <h2 class="text-4xl font-bold text-black">hi there</h2>
+            <p class="text-2xl text-black word">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quisquam!
+            </p>
+          </div>
+        </div>
+        <div
+          class="absolute top-0 bottom-0 flex items-center justify-center text-white right-10 bg-inherit"
+        >
+          <div class="flex flex-col items-center justify-center h-full text-center">
+            <img
+              src="../assets/image/webCover.jpg"
+              alt="Image 1"
+              class="object-cover w-full h-full p-5"
+            />
+          </div>
+        </div>
+      </div>
+    </div> -->
+
+<!-- <div class="relative group">
+      <img
+        src="../assets/image/webCover.jpg"
+        alt="Image 2"
+        class="object-cover w-full h-full rounded-lg"
+      />
+      <div
+        class="absolute inset-0 flex items-center justify-center text-white transition duration-300 ease-in-out bg-black opacity-0 group-hover:opacity-70"
+      >
+        <h2 class="text-xl font-bold">Content title 2</h2>
+        <p class="mt-2">This is a description of image 2.</p>
+      </div>
+    </div> -->
